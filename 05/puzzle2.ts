ffdaf0ae405ce628b62136201c4295c905269a37
Lovro Kunović <lovro_kunovic@hotmail.com>
2022-12-05 17:58:38 +0100

    Day 05

---
+++ 05/puzzle2.ts
@@ -0,0 +1,59 @@
+import fs from "fs";
+
+const contents = fs.readFileSync("input.txt").toString();
+const lines = contents.split("\r\n");
+
+let stacks = new Map();
+
+let instr = false;
+//load stacks
+for (let line of lines) {
+  if (line === "") {
+    instr = true;
+    //console.log(stacks);
+    continue;
+  }
+  if (!instr) {
+    if (line.startsWith(" 1")) continue;
+    for (let i = 1; ; i = i + 4) {
+      //console.log(i);
+      const char = line[i];
+
+      if (char === undefined) break;
+      if (char === " ") continue;
+
+      const index = Math.round(i / 4) + 1;
+
+      let stack = stacks.get(index);
+      if (!stack) stack = [];
+      stack.unshift(char);
+      stacks.set(index, stack);
+    }
+    // console.log();
+  } else {
+    const instruction = line
+      .replace("move ", "")
+      .replace("from ", "")
+      .replace("to ", "");
+
+    const [count, from, to] = instruction
+      .split(" ")
+      .map((num) => parseInt(num));
+    const stackFrom = stacks.get(from);
+    //console.log(stackFrom);
+    //break;
+    const stackTo = stacks.get(to);
+    const lastN = stackFrom.slice(-count);
+    stacks.set(from, stackFrom.slice(0, stackFrom.length - count));
+    stacks.set(to, stackTo.concat(lastN));
+  }
+}
+
+let string = "";
+for (let i = 0; i < Array.from(stacks.keys()).length; i++) {
+  string += stacks.get(i + 1).pop();
+}
+
+// console.log();
+// console.log(stacks);
+console.log(string);
